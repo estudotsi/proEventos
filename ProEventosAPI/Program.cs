1538a9c24de85ce51c336d1bbd37227edfd8aa73
Rogerio Martins Campos Martins Campos <zzmelzz@hotmail.com>
2023-05-25 15:00:47 -0300

    first

---
+++ ProEventosAPI/Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.EntityFrameworkCore;
+using ProEventosPersistence;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+var connString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ProEventosContext>
+	(opts =>
+	{
+		opts.UseMySql(connString, ServerVersion.AutoDetect(connString));
+	});
+
+builder.Services.AddControllers();
+builder.Services.AddCors();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger();
+	app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.UseCors
+	(
+	 cors => cors.AllowAnyHeader()
+				 .AllowAnyMethod()
+				 .AllowAnyOrigin()
+	);
+
+app.MapControllers();
+
+app.Run();
